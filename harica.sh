a328f8ad59603083a059debecd3bc45ef74f5cdf
Vincenzo Caracciolo <vincenzo.caracciolo@garr.it>
2025-04-17 15:38:41 +0200

    fix: renewal was not finding correct pem

--- harica.sh
+++ harica.sh
@@ -2,13 +2,12 @@
 
 source "$(dirname "$0")/utils.sh"
 
-CERT_PATH="$HARICA_OUTPUT_FOLDER/$DOMAIN.pem"
+CERT_FILENAME=$DOMAIN.crt
+FULLCHAIN_FILENAME=${DOMAIN}_fullchain.pem
+CERT_PATH="$HARICA_OUTPUT_FOLDER/$FULLCHAIN_FILENAME"
 RENEWAL_FORCED_FILE="$HARICA_OUTPUT_FOLDER/renewal_forced"
 
 renew_certificate() {
-    FULLCHAIN_FILENAME=${DOMAIN}_fullchain.pem
-    CERT_FILENAME=$DOMAIN.crt
-
     generate_and_download_certificate $FULLCHAIN_FILENAME $CERT_FILENAME
 
     # Post-request hook
@@ -19,11 +18,16 @@ renew_certificate() {
     echo "Certificate renewed for $DOMAIN."
 }
 
-if [ "$FORCE_RENEWAL" = true ] && [ ! -f "$RENEWAL_FORCED_FILE" ]; then
-    echo "Force renewal enabled. Renewing certificate for $DOMAIN..."
-    renew_certificate
-    # Create a file that indicates that renewal was forced
-    touch $RENEWAL_FORCED_FILE
+if [ "$FORCE_RENEWAL" = true ]; then
+    if [ ! -f "$RENEWAL_FORCED_FILE" ]; then
+        echo "Force renewal enabled. Renewing certificate for $DOMAIN..."
+        renew_certificate
+        # Create a file that indicates that renewal was forced
+        touch "$RENEWAL_FORCED_FILE"
+    else
+        echo -e "\nForce renewal was requested, but renewal was already forced previously."
+        echo -e "If you want to force renewal again, please remove: $RENEWAL_FORCED_FILE\n"
+    fi
 elif check_certificate_expiration "$CERT_PATH"; then
     renew_certificate
 else
